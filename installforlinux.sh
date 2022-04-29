ae6524b0fe1d9e56dea297ec5a458d1ae70fb980
Shourjjo Majumder <shourjjomajum@outlook.com>
2022-04-29 19:32:12 +0530

    Update installforlinux.sh

--- installforlinux.sh
+++ installforlinux.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-echo "Installing Python-ping"
+echo "Installing ping-bot version 1.0.0"
 echo "installing git"
 sudo apt-get install git
 echo "Cloning the repo"
 git clone https://github.com/shourgamer2/ping-bot
 echo "Installing python"
 sudo apt-get install python3.8
-echo "Installing pythonping"
-git clone https://github.com/alessandromaggio/pythonping
-cd pythonping
+echo "Installing pip"
+git clone https://github.com/pypa/pip
+cd pip
 sudo python3 setup.py install
+echo "Installing pythonping package"
+sudo pip install pythonping
 echo "Type cd ping-bot and then sudo python3 pingbotforlinux.py"
